d083048547cfb1ae61519ecaec74d179fdd9afa2
Grant Zheng <Grant Zheng@DESKTOP-D6N497R>
2019-01-18 00:56:16 -0500

    Finished arc and straight line plotting, started on zig-zag pattern.
    Started for loop, need to implement pattern on graph

--- HW1/Problem_3.py
+++ HW1/Problem_3.py
@@ -4,6 +4,8 @@ Created on Jan 17, 2019
 @author: Grant Zheng
 '''
 import matplotlib.pyplot as plt
+import numpy as np
+import math
 
 def readText(fileName):
     currFile = open(fileName, 'r')
@@ -13,41 +15,77 @@ def readText(fileName):
     l = len(splittedFile)
     for i in range(l):
         
-        try:
+        if (len(splittedFile[i]) != 0):
             firstChar = splittedFile[i][0]
             if (firstChar.isdigit()):
-                toReturnList = list(map(int, splittedFile[i].split(",")))
+                toReturnList = list(map(float, splittedFile[i].split(",")))
                 toReturn.append(toReturnList)
         
-        except:
-            pass
+        
 
     
     return toReturn
 
-def patternPlot():
-    return
+def patternPlot(lineSegment):
     
-def plotData(data):
+    lineType    = lineSegment[0]
+    patternType = lineSegment[5]
+    
+    
+        
+    if lineType == 1:
+        patternType = lineSegment[5]
+        xPos = [lineSegment[1], lineSegment[3]]
+        yPos = [lineSegment[2], lineSegment[4]]
+        if patternType == 1: 
+            plt.plot(xPos, yPos, 'k-')
+        
+        elif patternType == 3:
+            width = lineSegment[6]
+            pitch = lineSegment[7]
+            dir = [xPos[1] - xPos[0], yPos[1] - yPos[0]]
+            dirUnit = dir / np.linalg.norm(dir)
+            itr = np.linalg.norm(dir) / pitch
+            itr = math.floor(itr)
+            
+            for i in range (itr):
+                print("new Pattern")
+            print(dir)
+        
+    elif lineType == 2:
+        patternType = lineSegment[6]
+        resolution = 100
+        
+        if patternType == 1:
+            centerPos = lineSegment[1:3]
+            r         = lineSegment[3]
+            theta     = lineSegment[4:6] * 180
+            increment = (theta[1] - theta[0]) / resolution
+            
+            for i in range(100):
+                ang = theta[0] + increment * i
+                x1 = centerPos[0] + r * np.sin(np.pi * ang)
+                x2 = centerPos[0] + r * np.sin(np.pi * (ang + increment))
+                y1 = centerPos[1] + r * np.cos(np.pi * ang)
+                y2 = centerPos[1] + r * np.cos(np.pi * (ang + increment))
+                xPos = [x1, x2]
+                yPos = [y1, y2]
+                plt.plot(xPos, yPos, 'k-')
+        elif patternType == 3:
+            print("do")
+        
+        
+    
+def handlePlot(data):
     
     l = len(data)
     
     for i in range(l):
         currData = data[i]
-        lineType = currData[0]
-        linePattern = currData[6]
-        if (lineType == 1):
-            currDataX = [currData[1], currData[3]]
-            currDataY = [currData[2], currData[4]]
-            if (linePattern == 1):
-                plt.plot(currDataX, currDataY, 'k-')
-            elif(linePattern == 3):
-                return
-                
-        elif(currData[0] == 2):
-            center = [currData[1],currData[2]]
-               
+        patternPlot(currData)
+                         
     plt.show()
+    
 if __name__ == '__main__':
     textData = readText("2d-shape-1.txt")
-    plotData(textData)
+    handlePlot(textData)
