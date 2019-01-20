ea77aef703e55fa5ef0b6cbc8c89b1d1bf9b81e6
Grant Zheng <Grant Zheng@DESKTOP-D6N497R>
2019-01-20 00:54:05 -0500

    Finished zigzaging for arcs. Finished for problem 3.

--- HW1/Problem_3.py
+++ HW1/Problem_3.py
@@ -5,7 +5,7 @@ Created on Jan 17, 2019
 '''
 import matplotlib.pyplot as plt
 import numpy as np
-import math
+
 
 def readText(fileName):
     currFile = open(fileName, 'r')
@@ -20,10 +20,7 @@ def readText(fileName):
             if (firstChar.isdigit()):
                 toReturnList = list(map(float, splittedFile[i].split(",")))
                 toReturn.append(toReturnList)
-        
-        
 
-    
     return toReturn
 
 def patternPlot(lineSegment):
@@ -31,8 +28,6 @@ def patternPlot(lineSegment):
     lineType    = lineSegment[0]
     patternType = lineSegment[5]
     
-    
-        
     if lineType == 1:
         patternType = lineSegment[5]
         xPos = [lineSegment[1], lineSegment[3]]
@@ -83,11 +78,11 @@ def patternPlot(lineSegment):
     elif lineType == 2:
         patternType = lineSegment[6]
         resolution = 100
+        centerPos = lineSegment[1:3]
+        r         = lineSegment[3]
+        theta     = lineSegment[4:6] 
         
         if patternType == 1:
-            centerPos = lineSegment[1:3]
-            r         = lineSegment[3]
-            theta     = lineSegment[4:6] * 180
             increment = (theta[1] - theta[0]) / resolution
             
             for i in range(100):
@@ -99,8 +94,52 @@ def patternPlot(lineSegment):
                 xPos = [x1, x2]
                 yPos = [y1, y2]
                 plt.plot(xPos, yPos, 'k-')
+                
         elif patternType == 3:
-            print("do")
+            width = lineSegment[-2]
+            pitch = lineSegment[-1]
+            currAng = theta[0]
+            currPos = [np.cos(currAng * np.pi) * r, np.sin(currAng * np.pi) * r]
+            currPos = np.add(currPos, centerPos)
+            nextPos = [-1, -1]
+            nextAng = -1
+            angleSpan = theta[1] - theta[0]
+            totAng = 0
+            increment = np.arcsin((width / 2) / r) / 2 / np.pi
+            if (theta[1] < theta[0]):
+                angleSpan = 2 + angleSpan
+                
+            overAngle = 0
+            counter = 0
+            while(overAngle != 1):
+                
+                if (totAng + increment > angleSpan):
+                    overAngle = 1
+                    nextAng = theta[1]
+                    nextPos = [np.cos(nextAng * np.pi) * r,  np.sin(nextAng * np.pi) * r]
+                    nextPos = np.add(nextPos, centerPos)
+                else:
+                    nextAng = currAng + increment
+                    nextPos = [np.cos(nextAng * np.pi) * r,  np.sin(nextAng * np.pi) * r]
+                    nextPos = np.add(nextPos, centerPos)
+                    orthDir = np.subtract(nextPos, centerPos)
+                    unitOrthDir = orthDir / np.linalg.norm(orthDir)   
+                         
+                    if (counter % 4 == 2):
+                        nextPos = np.add(nextPos, unitOrthDir * width / 2)
+                    elif(counter % 4 == 0):
+                        nextPos = np.subtract(nextPos, unitOrthDir * width / 2)
+                    
+                toPlotX = [currPos[0], nextPos[0]]
+                toPlotY = [currPos[1], nextPos[1]]
+                currPos = nextPos
+                   
+                    
+                plt.plot(toPlotX, toPlotY, 'k-')
+                counter += 1
+                totAng += increment
+                currAng = nextAng
+            
         
         
     
@@ -115,5 +154,5 @@ def handlePlot(data):
     plt.show()
     
 if __name__ == '__main__':
-    textData = readText("2d-shape-1.txt")
+    textData = readText("2d-shape-2.txt")
     handlePlot(textData)
